a20b665d92b46e33c81106dfd990622d9f9b8d68
adamjogkins-eng <adamjogkins@gmail.com>
2026-03-06 08:13:15 +0000

    Update Swift tools version to 6.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "WinClone",
     platforms: [
-        .iOS(.v18) // We are officially targeting the latest and greatest
+        .iOS(.v18) 
     ],
     products: [
         .executable(name: "WinClone", targets: ["WinClone"])
